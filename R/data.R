f3af078fc30b6726fedee57460ce6e6b4c01a421
Taylor B. Arnold <taylor.arnold@acm.org>
2016-11-11 12:09:27 +0000

    version 0.24

---
+++ R/data.R
@@ -0,0 +1,42 @@
+#' Annotation of Barack Obama's State of the Union Addresses
+#'
+#' Parsed text from all eight State of the Union addresses given by Barack Obama.
+#'
+#' @name obama
+#' @docType data
+#' @references \url{http://www.presidency.ucsb.edu/sou.php}
+#' @keywords data
+NULL
+
+#' Universal Dependency Frequencies
+#'
+#' The language-specific frequency of universal dependency codes as given
+#' in standard treebank corpora. Frequencies are multiplied by 100.
+#'
+#' @name dep_frequency
+#' @docType data
+#' @references \url{http://universaldependencies.org/}
+#' @keywords data
+NULL
+
+#' Universal Part of Speech Code Frequencies
+#'
+#' The language-specific frequency of universal part of speech codes as given
+#' in standard treebank corpora. Frequencies are multiplied by 100.
+#'
+#' @name pos_frequency
+#' @docType data
+#' @references \url{http://universaldependencies.org/}
+#' @keywords data
+NULL
+
+#' Most frequent English words
+#'
+#' A dataset of the 150k most frequently used English words, extracted by Peter Norvig
+#' from the Google Web Trillion Word Corpus. Frequencies are multiplied by 100.
+#'
+#' @name word_frequency
+#' @docType data
+#' @references \url{http://norvig.com/ngrams/}
+#' @keywords data
+NULL
---
+++ R/download.R
@@ -0,0 +1,104 @@
+#' Download java files needed for cleanNLP
+#'
+#' The cleanNLP package does not supply the raw java files
+#' provided by the Stanford NLP Group as they are quite large.
+#' This function downloads the libraries automatically, by default into
+#' the directory where the package was installed.
+#'
+#' @importFrom           utils download.file unzip
+#' @importFrom           RCurl curlPerform CFILE close
+#' @param type           type of files to download. The base backage is always required.
+#'                       Other jars include model files for French, German,
+#'                       and Spanish. These can be installed in addition to the
+#'                       base package. By default, the function downloads the base
+#'                       package and English model files.
+#' @param output_loc     a string showing where the files are to be downloaded.
+#'                       If missing, will try to download files into the directory
+#'                       where the package was original installed.
+#' @param url            the url to try to download components from. Setting to NULL
+#'                       uses the default location on the Stanford NLP server, but
+#'                       you can set this manually by using this option. It also allows
+#'                       for local files, but note that the path must include the prefix
+#'                       \code{file://}. For details, see \code{\link{download.file}}.
+#' @param url_core       if \code{url} is not null, this flag indicates whehter the path
+#'                       given to url points to the core nlp files (which are zipped) or
+#'                       to model files (which are unzipped).
+#'
+#'@examples
+#'\dontrun{
+#'download_clean_nlp()
+#'download_clean_nlp(type="spanish")
+#'}
+#' @export
+download_clean_nlp = function(type = c("default", "base", "en", "fr", "de", "es"),
+    output_loc, url = NULL, url_core = TRUE) {
+
+  # set defaults and determine where files should be saved
+  baseURL <- "http://nlp.stanford.edu/software/"
+  coreFile <- "/stanford-corenlp-full-2016-10-31"
+  if (missing(output_loc)) {
+    output_loc <- system.file("extdata", package="cleanNLP")
+    if (file.access(output_loc, "6") == -1)
+      stop("You do not have read+write access to location where the",
+           "cleanNLP package is installed! You must specify an output",
+           "location with output_loc.")
+  }
+
+  # if url is given, simply download the specified files as required
+  if (!is.null(url)) {
+    if (url_core) {
+      ret <- download.file(url, destfile = file.path(output_loc, paste0(coreFile, ".zip")))
+      if (ret != 0) stop("Download error!")
+
+      unzip(file.path(output_loc, paste0(coreFile, ".zip")), exdir = output_loc)
+      file.remove(file.path(output_loc, paste0(coreFile, ".zip")))
+      return(0L)
+    } else {
+      fname <- basename(url)
+      download.file(url, destfile = file.path(output_loc, coreFile, fname))
+    }
+  }
+
+  # otherwise, determine what file types should be downloaded
+  type <- match.arg(type)
+
+  if (type %in% c("default", "base")) {
+    f <- RCurl::CFILE(file.path(output_loc, paste0(coreFile, ".zip")), mode="wb")
+    ret <- RCurl::curlPerform(url = paste0(baseURL, coreFile, ".zip"), writedata = f@ref, noprogress=FALSE)
+    RCurl::close(f)
+    if (ret != 0) stop("Download error!")
+
+    unzip(file.path(output_loc, paste0(coreFile, ".zip")), exdir = output_loc)
+    file.remove(file.path(output_loc, paste0(coreFile, ".zip")))
+    ret <- 0L
+  }
+
+  if (!file.exists(file.path(output_loc)))
+    stop("Must download base files to this location first! Set type='base'.")
+
+  if (type %in% c("default", "en")) {
+    f <- RCurl::CFILE(file.path(output_loc, coreFile, "/stanford-english-corenlp-2016-10-31-models.jar"), mode="wb")
+    ret <- RCurl::curlPerform(url = paste0(baseURL, "/stanford-english-corenlp-2016-10-31-models.jar"), writedata = f@ref, noprogress=FALSE)
+    RCurl::close(f)
+  }
+
+  if (type %in% c("fr")) {
+    f <- RCurl::CFILE(file.path(output_loc, coreFile, "/stanford-french-corenlp-2016-10-31-models.jar"), mode="wb")
+    ret <- RCurl::curlPerform(url = paste0(baseURL, "/stanford-french-corenlp-2016-10-31-models.jar"), writedata = f@ref, noprogress=FALSE)
+    RCurl::close(f)
+  }
+
+  if (type %in% c("de")) {
+    f <- RCurl::CFILE(file.path(output_loc, coreFile, "/stanford-german-corenlp-2016-10-31-models.jar"), mode="wb")
+    ret <- RCurl::curlPerform(url = paste0(baseURL, "/stanford-german-corenlp-2016-10-31-models.jar"), writedata = f@ref, noprogress=FALSE)
+    RCurl::close(f)
+  }
+
+  if (type %in% c("es")) {
+    f <- RCurl::CFILE(file.path(output_loc, coreFile, "/stanford-spanish-corenlp-2016-10-31-models.jar"), mode="wb")
+    ret <- RCurl::curlPerform(url = paste0(baseURL, "/stanford-spanish-corenlp-2016-10-31-models.jar"), writedata = f@ref, noprogress=FALSE)
+    RCurl::close(f)
+  }
+
+  ret
+}
